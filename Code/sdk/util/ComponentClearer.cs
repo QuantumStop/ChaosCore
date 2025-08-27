bfe242646dc67302e7d6fe4be99bb0362492d528
Roman <romanplay@mail.ru>
2025-08-28 01:06:42 +0300

    add util components to sdk and swap all props to gameprop

---
+++ Code/sdk/util/ComponentClearer.cs
@@ -0,0 +1,16 @@
+﻿[Title( "Clear All Components" )]
+[Description( "Clears all components on a gameobject including itself (to clear out hidden ones)" )]
+[Category( "Core" )]
+public class ComponentClear : Component, Component.ExecuteInEditor
+{
+	protected override void OnAwake()
+	{
+		base.OnStart();
+
+		foreach ( var p in this.Components.GetAll<Component>( FindMode.EverythingInSelf ) )
+		{
+			if ( p != this ) { Log.Info( p ); } // dont output self because you can tell
+			p.Destroy();
+		}
+	}
+}
