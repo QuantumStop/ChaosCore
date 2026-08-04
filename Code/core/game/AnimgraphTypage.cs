d0f232ea233d4798cf6ef25314767ba344791252
JohnFreeman <hljohnfreeman@gmail.com>
2026-08-04 18:33:31 +0300

    AG2: Push current glock + hands setup
    This is mostly functional ag2 setup for our viewmodel, ik solving is done in ag now, and ag is done vastly differently using a bunch of enums. Also uploaded ag1 file for look-up purposes/comparison.

---
+++ Code/core/game/AnimgraphTypage.cs
@@ -0,0 +1,11 @@
+namespace Core;
+
+public enum ViewmodelReloadStage : int
+{
+	reload_stage_ready = 0,
+	reload_stage_1 = 1,
+	reload_stage_2 = 2,
+	reload_stage_3 = 3,
+	reload_stage_4 = 4,
+	reload_stage_5 = 5
+}
